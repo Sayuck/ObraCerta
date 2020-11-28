67d296d9d0a7eb1bd4a6592f2a5d6215097f9e49
Roberto Nóbrega <nobrega.m.roberto@gmail.com>
2020-11-28 19:03:12 -0300

    erro de pagina nao encontrada adicionado

--- front/src/routes/index.js
+++ front/src/routes/index.js
@@ -9,7 +9,13 @@ import TermosDeUsuario from '../pages/TermosDeUsuario';
 import PerfilUsuario from '../pages/PerfilUsuario';
 import RecuperarSenha from '../pages/RecuperarSenha';
 import Sobre from '../pages/Sobre';
+import LinkQuebrado from '../assets/link-quebrado.svg';
 
+
+const Pagina404 = () => (<div>
+    <img src={LinkQuebrado} alt="Link-Quebrado"/>
+    <h1>404 Página não encontrada</h1>
+    </div>);
 
 class Routes extends Component {
 
@@ -31,7 +37,7 @@ class Routes extends Component {
 
     componentDidMount = () => {
         this.setState({
-            loggedIn: localStorage.getItem("loggedIn") || "NOT_LOGGED_IN", 
+            loggedIn: localStorage.getItem("loggedIn") || "NOT_LOGGED_IN",
             user: {
                 name: localStorage.getItem("name"),
                 telefone: localStorage.getItem("telefone"),
@@ -92,6 +98,10 @@ class Routes extends Component {
                             <Sobre {...props} loggedIn={this.state.loggedIn} />
                         )}
                     />
+
+                     <Route component={Pagina404}/>
+
+
                 </Switch>
             </BrowserRouter>
         );
